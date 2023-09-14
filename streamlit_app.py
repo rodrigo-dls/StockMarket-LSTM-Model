5ca2669b20f12f3600633eca41db36294e1a6d83
Rodrigo <rodri.m.dls@gmail.com>
2023-09-14 03:27:36 +0300

    Update streamlit_app.py
    Replaced template for a new one aligned with the project

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,35 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
+# Configuración de la página
+st.set_page_config(
+    page_title="App de Predicción de Acciones",
+    page_icon="✅",
+    # layout="wide",
+    # initial_sidebar_state="expanded"
+)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Encabezado
+st.title("App de Predicción de Acciones")
+st.write("Descripción breve de la aplicación...")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# Sección de Visualización del Dataset
+st.header("Visualización del Dataset")
+# Aquí puedes mostrar el dataset utilizando st.dataframe() o st.table()
 
-    points_per_turn = total_points / num_turns
+# Sección de Parámetros del Modelo de ML
+st.header("Parámetros del Modelo de ML")
+# Aquí puedes agregar widgets para que el usuario ingrese los parámetros del modelo.
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# Sección de Métricas
+st.header("Métricas del Modelo")
+# Aquí puedes mostrar las métricas de rendimiento del modelo.
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Sección de Visualización de Predicciones
+st.header("Visualización de Predicciones")
+# Aquí puedes agregar gráficos o visualizaciones de las predicciones del modelo.
+
+# Código para cargar y usar el modelo LSTM
+# (Esto puede incluirse según sea necesario)
+
+# Código para hacer predicciones con el modelo
+# (Esto puede incluirse según sea necesario)
